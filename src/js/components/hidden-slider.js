606d957eafc332ed95896c48629b868c0032edef
Evgeniy <87270911+evgenyBukharin@users.noreply.github.com>
2023-07-11 09:22:59 +0500

    init

---
+++ src/js/components/hidden-slider.js
@@ -0,0 +1,39 @@
+import Swiper, { Navigation } from "swiper";
+Swiper.use([Navigation]);
+
+const openSliderButtons = document.querySelectorAll(".open-slider-elem");
+const sliderContainer = document.querySelector(".hero__container-slider");
+const slider = document.querySelector(".hero__slider");
+openSliderButtons.forEach((btn) => {
+	btn.addEventListener("click", () => {
+		sliderContainer.classList.add("hero__container-slider-visible");
+	});
+});
+
+const swiper = new Swiper(slider, {
+	speed: 400,
+	centeredSlides: true,
+	slideToClickedSlide: true,
+	spaceBetween: 40,
+	breakpoints: {
+		769: {
+			slidesPerView: 3,
+		},
+		320: {
+			slidesPerView: 1,
+		},
+	},
+});
+
+slider.addEventListener("click", (e) => {
+	e.stopPropagation();
+});
+
+sliderContainer.addEventListener("click", () => {
+	sliderContainer.classList.remove("hero__container-slider-visible");
+});
+
+const closeSliderButton = document.getElementById("closeSliderButton");
+closeSliderButton.addEventListener("click", () => {
+	sliderContainer.classList.remove("hero__container-slider-visible");
+});
